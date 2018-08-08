bc3af60cf098ab51e8d748514954ae58d8829b22
Goutham Samanthula <sgr.goutham@gmail.com>
2018-08-08 07:16:57 -0400

    added image

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,9 +6,17 @@ margin: 0 auto;
 max-width: 650px;
 padding: 0 1rem`
 
+const Img = styled.img`
+display: block;
+margin-left: auto;
+margin-right: auto;
+width: 100%;
+height: 350px`
+
 class BaseLayout extends Component {
     render () {
         return <Div>
+            <Img src="https://source.unsplash.com/random/400x200" alt=""></Img>    
             <h3>MySweetSite</h3>
             {this.props.children}
         </Div>
